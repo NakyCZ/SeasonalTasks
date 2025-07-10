8b7bc0fa7bec15ec3381eb13b413b1a0274c671b
NakyCZ <61592155+NakyCZ@users.noreply.github.com>
2025-07-10 13:25:42 +0200

    Fix getMySQLManager symbol error

--- src/main/java/me/jacob/seasonaltasks/SeasonalTasks.java
+++ src/main/java/me/jacob/seasonaltasks/SeasonalTasks.java
@@ -3,30 +3,44 @@ package me.jacob.seasonaltasks;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.jacob.seasonaltasks.command.SeasonalTasksCommand;
 import me.jacob.seasonaltasks.data.DataHandler;
+import me.jacob.seasonaltasks.data.MySQLManager;
 import me.jacob.seasonaltasks.task.TaskManager;
 import me.jacob.seasonaltasks.listener.BlockListener;
+import me.jacob.seasonaltasks.listener.MenuListener;
 
 public class SeasonalTasks extends JavaPlugin {
 
     private static SeasonalTasks instance;
     private TaskManager taskManager;
     private DataHandler dataHandler;
+    private MySQLManager mySQLManager;
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+        
+        // Initialize MySQL if enabled
+        if (getConfig().getBoolean("mysql.enabled")) {
+            this.mySQLManager = new MySQLManager(this);
+            this.mySQLManager.connect();
+        }
+        
         this.taskManager = new TaskManager(this);
         this.dataHandler = new DataHandler(this);
 
         getCommand("seasonaltasks").setExecutor(new SeasonalTasksCommand(this));
         getServer().getPluginManager().registerEvents(new BlockListener(this), this);
+        getServer().getPluginManager().registerEvents(new MenuListener(this), this);
 
         getLogger().info("✅ SeasonalTasks enabled!");
     }
 
     @Override
     public void onDisable() {
+        if (mySQLManager != null) {
+            mySQLManager.disconnect();
+        }
         getLogger().info("🛑 SeasonalTasks disabled.");
     }
 
@@ -40,5 +54,9 @@ public class SeasonalTasks extends JavaPlugin {
 
     public DataHandler getDataHandler() {
         return dataHandler;
+    }
+    
+    public MySQLManager getMySQLManager() {
+        return mySQLManager;
     }
 }
